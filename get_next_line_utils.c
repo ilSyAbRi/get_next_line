ef0ec7b5d77ecdad0ed55856d52dcc43d0922bd4
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-28 17:49:42 +0100

    add helper function

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,88 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 14:29:44 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/28 14:29:57 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/28 17:47:13 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// helper function
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	ptr = malloc(len_s1 + len_s2 + 1);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memcpy(ptr, s1, len_s1);
+	ft_memcpy(ptr + len_s1, s2, len_s2);
+	ptr[len_s1 + len_s2] = '\0';
+	return (ptr);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+
+	if (s == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		ptr = malloc(1);
+		if (ptr == NULL)
+			return (NULL);
+		ptr[0] = '\0';
+		return (ptr);
+	}
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	ptr = malloc(sizeof(char) * len + 1);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memcpy(ptr, s + start, len);
+	ptr[len] = '\0';
+	return (ptr);
+}
