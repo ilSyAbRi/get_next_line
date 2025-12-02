9e48d0721016eea319d3a4b64bad8ce02758c162
ilSyAbRi <iliasssyabri1@gmail.com>
2025-12-02 17:01:41 +0100

    my current process i try to make better

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 14:05:31 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/30 18:53:32 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/12/02 17:01:23 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,29 @@
 
 char *get_next_line(int fd)
 {
-	static	char *stash = NULL;
-	char buffer[BUFFER_SIZE + 1];
-	int	bytes_read;
-	char*	tmp;
-	while ((bytes_read = read (fd,buffer,BUFFER_SIZE)) > 0)
+	static	char *stash;
+	char	buffer[BUFFER_SIZE + 1];
+	int	read_byte;
+	char	*tmp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return NULL;
+	if (stash == NULL)
 	{
-		buffer[bytes_read] = '\0';
+		stash = malloc(1);
+		if (stash == NULL)
+			return NULL;
+		stash[0] = '\0';
+	}
+	while (read_byte = read(fd,buffer,BUFFER_SIZE) > 0)
+	{
+		buffer[read_byte] = '\0';
 		tmp = stash;
 		stash = ft_strjoin(stash,buffer);
 		free(tmp);
-		if (ft_strchr(stash,'\n'))
+		if (ft_strchr(stash,'\n'));
 			break;
 	}
-
 	/*line = extract_line(stash);
 	stash = clean_stash(stash);
 	return line;*/
