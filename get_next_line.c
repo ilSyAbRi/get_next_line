a9d534d26314eb9b7ea2fd71e4d5745fbe307d30
ilSyAbRi <iliasssyabri1@gmail.com>
2025-12-03 00:14:57 +0100

    add extract function

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,52 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 14:05:31 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/12/02 18:23:41 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/12/03 00:14:38 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*extract_line(char *stash)
 {
-	static	char *stash;
-	char	buffer[BUFFER_SIZE + 1];
-	int	read_byte;
-	char	*tmp;
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	j = 0;
+	if (stash == NULL || stash[0] == '\0')
+		return (NULL);
+	while (stash[i] != '\0' && stash[i] != '\n')
+		i++;
+	line = malloc(i + (stash[i] == '\n') + 1);
+	if (line == NULL)
+		return (NULL);
+	while (j < i + (stash[i] == '\n'))
+	{
+		line[j] = stash[j];
+		j++;
+	}
+	line[i + (stash[i] == '\n')] = '\0';
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		buffer[BUFFER_SIZE + 1];
+	int			read_byte;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return NULL;
-	while ((read_byte = read(fd,buffer,BUFFER_SIZE)) > 0)
+		return (NULL);
+	while ((read_byte = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[read_byte] = '\0';
-		stash = ft_strjoin(stash,buffer);
-		if (ft_strchr(stash,'\n'))
-			break;
+		stash = ft_strjoin(stash, buffer);
+		if (ft_strchr(stash, '\n'))
+			break ;
 	}
-	/*line = extract_line(stash);
-	stash = clean_stash(stash);
-	return line;*/
-	return stash;
+	line = extract_line(stash);
+	// stash = clean_stash(stash);
+	return (line);
 }
